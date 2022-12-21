762a36ac56e085fd1f0c77eebe2d9593457383a8
kim_suneun <tjsdms89@gmail.com>
2022-12-22 03:28:51 +0900

    dev 머지 작업용

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,10 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Login from './pages/Login/login';
-import Register from './pages/Register/register';
+import Home from './pages/Home/home';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store.js';
-import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './style/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <Provider store={store}>
         <App />
       </Provider>
