324c87ba363734c7df4734276946152e293095ab
hyojung <liv22developer@gmail.com>
2022-12-21 19:32:13 +0900

    feat: 헤더 초기레이아웃 구현

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import GlobalStyle from './style/GlobalStyle';
-import Home from './pages/Home/home';
+import Home from './pages/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Header />
       <Home />
     </div>
   );
