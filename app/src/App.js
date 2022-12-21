0333c623890c53777dffd058505cd5f91b58631b
kim_suneun <tjsdms89@gmail.com>
2022-12-22 03:51:34 +0900

    헤더와 링크 설정

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/home';
+import Login from './pages/Login/login';
+import MyPage from './pages/MyPage/MyPage';
+import Register from './pages/Register/register';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/mypage" element={<MyPage />} />
+    </Routes>
   );
 }
 
