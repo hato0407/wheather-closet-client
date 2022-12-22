218eeab04a4ecc701058484962d8c14dda191013
이 효정 <rmr02090@gmail.com>
2022-12-22 07:01:16 +0000

    feat: 모달 토글기능 추가 #6

--- app/src/components/Avartar/AvatarModal.jsx
+++ app/src/components/Avartar/AvatarModal.jsx
@@ -1,19 +1,20 @@
+import Avartar from './Avatar';
 import * as S from './AvatarModal.style';
 
-export default function AvartarModal() {
+export default function AvartarModal({ onClick }) {
   return (
     <S.AvartarModalLayout>
       <S.Container>
         <S.Wrapper>
-          <S.Preview>이미지 미리보기</S.Preview>
+          <Avartar />
           <S.Buttons>
             <S.Label htmlFor="upload">이미지 업로드</S.Label>
-            <input type="file" name="" id="upload" />
+            <input type="file" id="upload" accept="image/jpg, image/png" />
             <S.Button>이미지 삭제</S.Button>
           </S.Buttons>
         </S.Wrapper>
-        <S.SaveButton>저장</S.SaveButton>
-        <S.CloseButton>X</S.CloseButton>
+        <S.SaveButton onClick={onClick}>완료</S.SaveButton>
+        <S.CloseButton onClick={onClick}>X</S.CloseButton>
       </S.Container>
     </S.AvartarModalLayout>
   );
