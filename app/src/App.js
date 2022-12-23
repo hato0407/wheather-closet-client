579992008b95378bc7e8be69bac5c41fb6602105
kim_suneun <tjsdms89@gmail.com>
2022-12-23 18:52:15 +0900

    작성하기 미리보기 추가

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,11 @@ import Home from './pages/Home/home';
 import Login from './pages/Login/login';
 import MyPage from './pages/MyPage/MyPage';
 import Register from './pages/Register/register';
-import Write from './pages/Write/write';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Write />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/mypage" element={<MyPage />} />
