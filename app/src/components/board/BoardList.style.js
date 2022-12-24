0e3e4bf570b51e76a6fab5f45d3be6df95f03ae8
hyojung <rmr02090@gmail.com>
2022-12-24 21:12:33 +0900

    design: 스켈레톤UI 추가

--- app/src/components/board/BoardList.style.js
+++ app/src/components/board/BoardList.style.js
@@ -21,23 +21,3 @@ export const ClothesImage = styled.img`
   max-width: 100%;
   height: auto;
 `;
-
-export const HeartWrapper = styled.div`
-  position: absolute;
-  bottom: 10px;
-  right: 30px;
-
-  > span + span {
-    margin-left: 4px;
-  }
-`;
-
-// HeartIcon(임시)
-export const HeartIcon = styled.span`
-  font-size: 20px;
-`;
-
-export const HeartCount = styled.span`
-  margin-left: 4px;
-  font-size: 20px;
-`;
