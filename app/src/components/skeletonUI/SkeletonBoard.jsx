19c5b336fe6d2b7d7cceb7a4861e626946bb6456
hyojung <liv22developer@gmail.com>
2022-12-24 21:12:33 +0900

    design: 스켈레톤UI 추가

--- app/src/components/skeletonUI/SkeletonBoard.jsx
+++ app/src/components/skeletonUI/SkeletonBoard.jsx
@@ -1,5 +1,15 @@
 import * as S from './SkeletonBoard.style';
 
 export default function SkeletonBoard() {
-  return <div></div>;
+  return (
+    <S.SkeletonBoardLayout>
+      {new Array(9).fill(1).map((item) => (
+        <ListItem key={item} />
+      ))}
+    </S.SkeletonBoardLayout>
+  );
+}
+
+function ListItem() {
+  return <S.Li></S.Li>;
 }
--- app/src/components/skeletonUI/SkeletonBoard.style.js
+++ app/src/components/skeletonUI/SkeletonBoard.style.js
@@ -1,1 +1,38 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+// List(ul)
+export const SkeletonBoardLayout = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px 20px;
+`;
+
+// Item(li)
+const loading = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  50%,
+  100% {
+    transform: translateX(307px);
+  }
+`;
+
+export const Li = styled.li`
+  position: relative;
+  width: 307px;
+  min-height: 307px;
+  background-color: #c1c1c1;
+  overflow: hidden;
+
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 80px;
+    height: 100%;
+    background: linear-gradient(to right, transparent, #d7d7d7, transparent);
+    animation: ${loading} 2s infinite;
+  }
+`;
