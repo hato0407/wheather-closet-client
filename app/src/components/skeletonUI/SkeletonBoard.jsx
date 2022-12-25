095f9f5afb93bac017a993ba371c13e2123b80f5
hyojung <rmr02090@gmail.com>
2022-12-25 19:10:01 +0900

    fix(board): key idx 누락 수정

--- app/src/components/skeletonUI/SkeletonBoard.jsx
+++ app/src/components/skeletonUI/SkeletonBoard.jsx
@@ -3,8 +3,8 @@ import * as S from './SkeletonBoard.style';
 export default function SkeletonBoard() {
   return (
     <S.SkeletonBoardLayout>
-      {new Array(9).fill(1).map((item) => (
-        <ListItem key={item} />
+      {new Array(9).fill(1).map((item, idx) => (
+        <ListItem key={idx} />
       ))}
     </S.SkeletonBoardLayout>
   );
