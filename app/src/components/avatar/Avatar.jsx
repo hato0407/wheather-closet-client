9f9a257bb0628ddacbccf9d689bac982d8285663
이 효정 <rmr02090@gmail.com>
2022-12-26 05:12:00 +0000

    feat(com-avartar): 이미지 업로드 , 삭제 #18

--- app/src/components/avatar/Avatar.jsx
+++ app/src/components/avatar/Avatar.jsx
@@ -1,15 +1,15 @@
 import * as S from './Avatar.style';
 import defalutProgfile from '../../assets/images/default-profile.png';
 
-export default function Avartar({ toggleMenu, toggleModal }) {
+export default function Avartar({ toggleMenu, toggleModal, previewAvartar }) {
   return (
     <S.AvatarLayout>
       <S.AvatarImage
-        src={defalutProgfile}
+        src={previewAvartar ? previewAvartar : defalutProgfile}
         alt="프로필 이미지"
         onClick={() => {
           toggleMenu();
-          toggleModal();
+          // toggleModal();
         }}
       />
     </S.AvatarLayout>
