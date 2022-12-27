58d6d91ccacb7064aa95193810e287c23933e0c8
sora02 <leeseul1914@naver.com>
2022-12-28 01:18:43 +0900

    온도 변화에 따른 텍스트 변화 완성

--- app/src/config/config.js
+++ app/src/config/config.js
@@ -4,3 +4,46 @@ export const API = {
   YOUTUBE_EMBED: 'https://youtube.com/embed/',
   WEATHER_ICON: 'http://openweathermap.org/img/w/',
 };
+
+export const DescTemperature = {
+  1: {
+    temperature: 28,
+    desc: '무더운 날씨네요. 장시간 야외활동에 주의하세요!',
+    clothes: '민소매, 숏팬츠 등',
+  },
+  2: {
+    temperature: 27,
+    desc: '더운 온도네요. 아이스크림 하나 어떠신가요?',
+    clothes: '티셔츠, 반바지 등',
+  },
+  3: {
+    temperature: 22,
+    desc: '선선한 날씨네요. 너무 얇게 입지 않도록 조심하세요!',
+    clothes: '셔츠, 면바지 등',
+  },
+  4: {
+    temperature: 19,
+    desc: '약간 쌀쌀한 날씨예요. 외투를 잊지마세요!',
+    clothes: '후드티, 바람막이, 슬랙스 등',
+  },
+  5: {
+    temperature: 16,
+    desc: '추운 가을 날씨네요. 가디건을 입기 딱 좋습니다.',
+    clothes: '기모후드티, 가디건, 니트 등',
+  },
+  6: {
+    temperature: 11,
+    desc: '이제는 많이 추워졌습니다. 코트의 계절이네요.',
+    clothes: '야상, 자켓, 트렌치코트 등',
+  },
+  7: {
+    temperature: 8,
+    desc: '점점 한겨울이 다가오고 있습니다. 감기 조심하세요!',
+    clothes: '가죽자켓, 코트 등',
+  },
+  8: {
+    temperature: 5,
+    desc: '목도리, 귀마개 등 방한용품 필수! 보온내의가 필요할 때 입니다!',
+    clothes: '두꺼운 코트, 패딩 등',
+  },
+};
