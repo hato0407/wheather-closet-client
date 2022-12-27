4ade7abdd1c307a12af34138ad13fb6f7e66f616
이 효정 <rmr02090@gmail.com>
2022-12-27 05:56:39 +0000

    Revert "Revert "Merge branch 'feature-login' into 'dev'""
    This reverts commit 9f4f778e1b58308836cff115a3ba87822bb03788

--- app/src/pages/Login/login.jsx
+++ app/src/pages/Login/login.jsx
@@ -1,7 +1,8 @@
-import styled from 'styled-components';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import GoogleButton from './googleLogin';
+import * as L from './Login.style';
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
@@ -9,150 +10,43 @@ export default function Login() {
 
   return (
     <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-      <Container>
-        <Container2>
-          <TitleWrapper>
-            <Title>날씨 옷장</Title>
-            <SubTitle>오늘 날씨엔 어떻게 입을까?</SubTitle>
-          </TitleWrapper>
-          <Input
+      <L.Container>
+        <L.Container2>
+          <L.TitleWrapper>
+            <L.Title>날씨 옷장</L.Title>
+            <L.SubTitle>오늘 날씨엔 어떻게 입을까?</L.SubTitle>
+          </L.TitleWrapper>
+          <L.Input
             type="email"
             {...register('email', { required: true })}
             placeholder="이메일"
           />
 
-          <Input
+          <L.Input
             type="password"
             {...register('password', { required: true, minLength: 4 })}
             placeholder="비밀번호"
           />
 
-          <Button type="submit">로그인</Button>
-          <DividerLine />
-          <LoginWrapper>
-            <Description2>구글로 로그인</Description2>
-            <Description2>비밀번호를 잊으셨나요?</Description2>
-          </LoginWrapper>
-        </Container2>
-        <Container3>
-          <Description>아직 계정이 없다면?</Description>
+          <L.Button type="submit">로그인</L.Button>
+          <L.DividerLine />
+          <L.LoginWrapper>
+            <L.Description2>
+              <GoogleButton></GoogleButton>
+            </L.Description2>
+            <L.Description2>비밀번호를 잊으셨나요?</L.Description2>
+          </L.LoginWrapper>
+        </L.Container2>
+        <L.Container3>
+          <L.Description>아직 계정이 없다면?</L.Description>
 
-          <Description>
+          <L.Description>
             <Link to="/register">가입하기</Link>
-          </Description>
+          </L.Description>
 
-          <Description>{data}</Description>
-        </Container3>
-      </Container>
+          <L.Description>{data}</L.Description>
+        </L.Container3>
+      </L.Container>
     </form>
   );
 }
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-`;
-
-const Container2 = styled.div`
-  border: 2px solid #f0f1f3;
-  border-radius: 3px;
-  width: 400px;
-  height: 450px;
-  box-sizing: border-box;
-  padding: 28px 24px 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-`;
-
-const Container3 = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 2px solid #f0f1f3;
-  border-radius: 3px;
-  width: 400px;
-  height: 70px;
-  margin: 20px 0px 20px 0px;
-`;
-
-const TitleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const Title = styled.h2`
-  font-weight: bold;
-  font-size: 40px;
-  color: #151618;
-  margin: 0;
-  margin-top: 20px;
-`;
-
-const SubTitle = styled.h4`
-  color: #5e5f61;
-  font-size: 14px;
-  margin: 0;
-  opacity: 0.7;
-`;
-
-const Description = styled.p`
-  color: #5e5f61;
-  font-size: 14px;
-  font-weight: bold;
-
-  + p {
-    margin-left: 10px;
-    color: #4b7bf4;
-  }
-`;
-
-const Description2 = styled.p`
-  color: #2063b1;
-  font-size: 14px;
-  font-weight: bold;
-  margin: 0;
-  margin-bottom: 20px;
-`;
-
-const Input = styled.input`
-  width: 310px;
-  height: 38px;
-  padding-left: 10px;
-  opacity: 0.7;
-  + input {
-    margin-top: 10px;
-  }
-`;
-
-const Button = styled.button`
-  width: 320px;
-  height: 35px;
-  margin: 20px 0px 20px 0px;
-  background-color: #fff500;
-  border: solid #ffffff;
-  border-radius: 8px;
-  opacity: 0.5;
-`;
-
-const DividerLine = styled.div`
-  width: 310px;
-  height: 2px;
-  background-color: #ececec;
-  position: absolute;
-  bottom: 120px;
-`;
-
-const LoginWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 70px;
-  margin-top: auto;
-`;
