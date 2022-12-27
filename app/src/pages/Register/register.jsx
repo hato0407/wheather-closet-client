ffc1750bbdd1624fd419ed24250781364f6298b8
hyojung <liv22developer@gmail.com>
2022-12-27 14:50:48 +0900

    Revert "Merge branch 'feature-login' into 'dev'"
    This reverts commit d7a0c19768d585918601c9431c578eeea4c3bb23, reversing
    changes made to d124cf664ad1659ba48f4e2f752dcb6d65bab066.

--- app/src/pages/Register/register.jsx
+++ app/src/pages/Register/register.jsx
@@ -1,4 +1,4 @@
-import * as L from './Register.style';
+import styled from 'styled-components';
 import { useState, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
@@ -17,26 +17,26 @@ export default function Register() {
 
   return (
     <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-      <L.Container>
-        <L.Container2>
-          <L.TitleWrapper>
-            <L.Title>날씨 옷장</L.Title>
-            <L.SubTitle>날씨에 맞는 옷을 추천해드려요.</L.SubTitle>
-            <L.RButton>구글로 로그인</L.RButton>
-          </L.TitleWrapper>
-          <L.Input
+      <Container>
+        <Container2>
+          <TitleWrapper>
+            <Title>날씨 옷장</Title>
+            <SubTitle>날씨에 맞는 옷을 추천해드려요.</SubTitle>
+            <RButton>구글로 로그인</RButton>
+          </TitleWrapper>
+          <Input
             type="email"
             {...register('email', { required: true })}
             placeholder="이메일 주소"
           />
 
-          <L.Input
+          <Input
             type="password"
             {...register('password', { required: true, minLength: 4 })}
             placeholder="비밀번호"
           />
 
-          <L.Input
+          <Input
             type="password"
             {...register('password_confirm', {
               required: true,
@@ -45,28 +45,131 @@ export default function Register() {
             placeholder="비밀번호 확인"
           />
 
-          <L.Input
+          <Input
             type="text"
             {...register('nickname', { required: true, maxLength: 10 })}
             placeholder="닉네임"
           />
 
-          <L.LButton type="submit">가입</L.LButton>
+          <LButton type="submit">가입</LButton>
 
           {errors.password_confirm && (
-            <L.Description>비밀번호가 일치하지 않습니다.</L.Description>
+            <Description>비밀번호가 일치하지 않습니다.</Description>
           )}
-        </L.Container2>
-        <L.Container3>
-          <L.Description>계정이 있으신가요?</L.Description>
+        </Container2>
+        <Container3>
+          <Description>계정이 있으신가요?</Description>
 
-          <L.Description>
+          <Description>
             <Link to="/login">로그인</Link>
-          </L.Description>
+          </Description>
 
-          <L.Description>{data}</L.Description>
-        </L.Container3>
-      </L.Container>
+          <Description>{data}</Description>
+        </Container3>
+      </Container>
     </form>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Container2 = styled.div`
+  border: 2px solid #f0f1f3;
+  border-radius: 3px;
+  width: 400px;
+  height: 560px;
+  box-sizing: border-box;
+  padding: 28px 24px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+const Container3 = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid #f0f1f3;
+  border-radius: 3px;
+  width: 400px;
+  height: 70px;
+  margin: 20px 0px 20px 0px;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Title = styled.h2`
+  font-weight: bold;
+  font-size: 40px;
+  color: #151618;
+  margin: 0;
+  margin-top: 20px;
+`;
+
+const SubTitle = styled.h4`
+  color: #5e5f61;
+  font-size: 14px;
+  margin: 0px 0px 20px 0px;
+  opacity: 0.7;
+`;
+
+const Description = styled.p`
+  color: #5e5f61;
+  font-size: 14px;
+  font-weight: bold;
+
+  + p {
+    margin-left: 10px;
+    color: #4b7bf4;
+    cursor: pointer;
+  }
+`;
+
+const Input = styled.input`
+  width: 310px;
+  height: 38px;
+  padding-left: 10px;
+  opacity: 0.7;
+  + input {
+    margin-top: 25px;
+  }
+`;
+
+const LButton = styled.button`
+  width: 320px;
+  height: 40px;
+  margin: 20px 0px 20px 0px;
+  background-color: #fff500;
+  border: solid #ffffff;
+  border-radius: 8px;
+  opacity: 0.5;
+`;
+
+const RButton = styled.button`
+  width: 320px;
+  height: 35px;
+  background-color: #fff500;
+  border: solid #ffffff;
+  border-radius: 8px;
+  opacity: 0.8;
+`;
+
+const DividerLine = styled.div`
+  width: 310px;
+  height: 2px;
+  background-color: #ececec;
+  position: absolute;
+  bottom: 260px;
+`;
--- app/src/pages/Write/write.jsx
+++ app/src/pages/Write/write.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import styled from 'styled-components';
-import * as L from './Write.style';
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
-import { clothsType, styleType } from './ClothsOptions';
+import { clothsOptions } from './ClothsOptions';
 
 export default function Write() {
   const { control, handleSubmit, register, watch } = useForm();
@@ -25,52 +24,53 @@ export default function Write() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <L.ContainerWrapper>
-        <L.Container>
-          <L.SubTitle>새 게시물 만들기</L.SubTitle>
-          <L.ContainerBox>
-            <L.Container2>
-              <img
-                //src={avatar.length > 1 ? { preview } : './select.png'}
-                src={preview}
-                alt="select_image"
-                width={500}
-                height={400}
-              />
-              <L.SeleteButton type="button">
-                <L.Label for="picture">사진 선택하기</L.Label>
-              </L.SeleteButton>
-              <L.InputImg
+      <ContainerWrapper>
+        <Container>
+          <SubTitle>새 게시물 만들기</SubTitle>
+          <ContainerBox>
+            <Container2>
+              <img src={preview} alt="select_image" width={500} height={400} />
+              <SeleteButton type="button">
+                <Label for="picture">사진 선택하기</Label>
+              </SeleteButton>
+              <InputImg
                 {...register('avatar')}
                 id="picture"
                 name="avatar"
                 type="file"
                 accept="image/*"
               />
-            </L.Container2>
-            <L.Container2>
-              <L.UserName>
-                <L.Title>유저 닉네임</L.Title>
-              </L.UserName>
+            </Container2>
+            <Container2>
+              <UserName>
+                <Title>유저 닉네임</Title>
+              </UserName>
 
-              <L.UserWrite>
-                <L.Description>위치 추가</L.Description>
-              </L.UserWrite>
-              <L.UserWrite>
-                <L.Description>
-                  스타일 구분
+              <UserWrite>
+                <Description>위치 추가</Description>
+              </UserWrite>
+              <UserWrite>
+                <Description>
+                  <label>스타일 구분</label>
                   <Controller
                     name="styleType"
                     render={({ field }) => (
-                      <Select {...field} options={styleType} />
+                      <Select
+                        {...field}
+                        options={[
+                          { value: 'casual', label: '캐쥬얼' },
+                          { value: 'street', label: '스트릿' },
+                          { value: 'formal', label: '포멀' },
+                        ]}
+                      />
                     )}
                     control={control}
                     defaultValue=""
                   />
-                </L.Description>
-              </L.UserWrite>
-              <L.UserWrite>
-                <L.Description>
+                </Description>
+              </UserWrite>
+              <UserWrite>
+                <Description>
                   옷 종류 태그
                   <Controller
                     name="clothsType"
@@ -78,7 +78,7 @@ export default function Write() {
                       <Select
                         {...field}
                         closeMenuOnSelect={false}
-                        options={clothsType}
+                        options={clothsOptions}
                         components={animatedComponents}
                         isMulti
                       />
@@ -86,14 +86,112 @@ export default function Write() {
                     control={control}
                     defaultValue=""
                   />
-                </L.Description>
-              </L.UserWrite>
+                </Description>
+              </UserWrite>
 
-              <L.Button type="submit">공유하기</L.Button>
-            </L.Container2>
-          </L.ContainerBox>
-        </L.Container>
-      </L.ContainerWrapper>
+              <Button type="submit">공유하기</Button>
+            </Container2>
+          </ContainerBox>
+        </Container>
+      </ContainerWrapper>
     </form>
   );
 }
+const ContainerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 1100px;
+  height: 650px;
+  box-shadow: rgba(149, 157, 165, 0.3) 0px 8px 24px;
+`;
+
+const ContainerBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container2 = styled.div`
+  border: 2px solid #f0f1f3;
+  border-radius: 3px;
+  width: 600px;
+  height: 500px;
+  box-sizing: border-box;
+  padding: 28px 24px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+
+  + div {
+    width: 340px;
+    margin-left: 40px;
+  }
+`;
+
+const UserName = styled.div`
+  width: 300px;
+  height: 30px;
+  margin-bottom: 80px;
+`;
+
+const UserWrite = styled.div`
+  width: 300px;
+  height: 50px;
+  margin: 20px 0px 20px 0px;
+  border-bottom: solid 1px #ececec;
+`;
+
+const Title = styled.h5`
+  color: #5e5f61;
+  margin: 0;
+  margin-top: 20px;
+`;
+
+const SubTitle = styled.h4`
+  color: #5e5f61;
+  font-size: 20px;
+  opacity: 0.8;
+`;
+
+const Description = styled.p`
+  color: #5e5f61;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const SeleteButton = styled.button`
+  height: 35px;
+  margin: 0px 0px 20px 0px;
+  background-color: #fff500;
+  border: solid #ffffff;
+  border-radius: 8px;
+  opacity: 0.7;
+`;
+
+const Button = styled.button`
+  height: 35px;
+  margin-top: 45px;
+  background-color: #fff500;
+  border: solid #ffffff;
+  border-radius: 8px;
+  opacity: 0.7;
+`;
+
+const InputImg = styled.input`
+  width: 0;
+  height: 0;
+`;
+
+const Label = styled.label`
+  cursor: pointer;
+`;
