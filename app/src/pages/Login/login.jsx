28e636f3682fb982ae40e124f0800c35332e26f9
kim_suneun <tjsdms89@gmail.com>
2022-12-30 00:21:56 +0900

    로그인 api 추가

--- app/src/pages/Login/login.jsx
+++ app/src/pages/Login/login.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import GoogleButton from './googleLogin';
 import * as L from './Login.style';
+import axios from 'axios';
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
   const [data, setData] = useState('');
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    const res = await axios.post('/api/user/login', data);
+    const token = res.token;
+    console.log('res', res);
+
+    localStorage.setItem('token', token);
+
+    alert(`정상적으로 로그인되었습니다.`);
+
+    navigate('/');
+  };
 
   return (
-    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <L.Container>
         <L.Container2>
           <L.TitleWrapper>
